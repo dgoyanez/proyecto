88c0ad9611a9e5ee74cd97eb8ff3aa4034059063
dgoyanez <106639113+dgoyanez@users.noreply.github.com>
2022-09-24 22:37:32 -0300

    v2

--- client/src/components/Pago.jsx
+++ client/src/components/Pago.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
+import {useNavigate} from 'react-router';
 
 const Pago = () => {
-  return (
-      <div>
-        <div className='block'>
-        <p className='sub1'>pago tarjeta</p>
+
+const navigate = useNavigate();
+const volverHome =()=>{
+    navigate(`/servicios`);
+}
+
+return (
+    <div className='blockpago'>
+        <div>
+            <h1 className='titlepago d-flex justify-content-center'>PROCESANDO PAGO DE SERVICIO</h1>
         </div>
-        <button>voler home</button>
-      </div>
-  )
+        <div className='block'>
+            <div>
+                <p className='sub1'>seleccionar tarjeta</p>
+            </div>
+            <div>
+                <p>card number</p>
+            </div>
+            <div>
+                <p>expiration date</p>
+            </div>
+            <div>
+                <p>full name</p>
+            </div>
+        </div>
+        <div className='btns d-flex'>
+            <button className='btnpago' onClick={volverHome}>voler home</button>
+            <button className='btnpago2'>COMPRAR SERVICIO</button>
+        </div>
+            
+    </div>
+)
 }
 
 export default Pago;
